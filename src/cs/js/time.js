55ca3138ab38fdac1421fc7239c576b5a4175b09
guochunpeng666 <guochunpeng666@163.com>
2019-06-26 22:38:33 +0800

    e

--- src/cs/js/time.js
+++ src/cs/js/time.js
@@ -8,7 +8,7 @@ $(function () {
         t_div.innerHTML = "填表时间" + now.getFullYear()
          + "年" + (now.getMonth() + 1) + "月" + now.getDate()
          + "日" + now.getHours() + "时" + now.getMinutes()
-         + "分" + now.getSeconds() + "秒";
+         + "分" + now.getSeconds() +"秒";
         //等待一秒钟后调用time方法，由于settimeout在time方法内，所以可以无限调用
         setTimeout(time, 1000);
     }
